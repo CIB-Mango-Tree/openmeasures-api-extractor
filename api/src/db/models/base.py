434e06b8632e31f998d3c3e1617c523780a5ccd3
VEDA95 <snetterfield1@gmail.com>
2025-09-01 13:12:55 -0400

    Changed primary key to use the appropriate datatype

--- api/src/db/models/base.py
+++ api/src/db/models/base.py
@@ -1,6 +1,7 @@
 from sqlalchemy import UUID
+from uuid import uuid4
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
-    id: Mapped[str] = mapped_column(UUID, primary_key=True)
+    id: Mapped[UUID] = mapped_column(UUID, primary_key=True, default=uuid4())
