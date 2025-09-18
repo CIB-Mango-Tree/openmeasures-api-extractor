9893d826432404fe4acd6d9d2c5f6c8d2fcb1f42
VEDA95 <snetterfield1@gmail.com>
2025-09-18 06:48:30 -0400

    Modified models after discussing limit tracking with ben on slack

--- api/src/db/models/limit.py
+++ api/src/db/models/limit.py
@@ -1,8 +1,9 @@
-from sqlalchemy import INTEGER, DateTime
+from sqlalchemy import INTEGER, DateTime, FLOAT
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class QueryLimit(DeclarativeBase):
     count: Mapped[INTEGER] = mapped_column(INTEGER, nullable=False, default=39)
+    percentage: Mapped[FLOAT] = mapped_column(FLOAT, nullable=False, default=0)
     previous_request_date: Mapped[DateTime] = mapped_column(DateTime, nullable=True)
     limit_refresh_date: Mapped[DateTime] = mapped_column(DateTime, nullable=True)
--- api/src/db/models/query.py
+++ api/src/db/models/query.py
@@ -1,7 +1,7 @@
 from .base import BaseWithTimestamp
 from .term import QueryTerm
 from .request import QueryRequest
-from sqlalchemy import DateTime, VARCHAR, INTEGER
+from sqlalchemy import DateTime, VARCHAR, INTEGER, FLOAT
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import List
 
@@ -14,11 +14,7 @@ class Query(BaseWithTimestamp):
     start_date: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
     end_date: Mapped[DateTime] = mapped_column(DateTime, nullable=False)
     platform: Mapped[VARCHAR] = mapped_column(VARCHAR(64), nullable=False)
-    row_count: Mapped[INTEGER] = mapped_column(INTEGER, nullable=False)
-    rows_fetched: Mapped[INTEGER] = mapped_column(INTEGER, nullable=False)
+    rows_fetched: Mapped[INTEGER] = mapped_column(INTEGER, nullable=False, default=0)
+    percentage: Mapped[FLOAT] = mapped_column(FLOAT, nullable=False, default=0)
     terms: Mapped[List[QueryTerm]] = relationship()
     requests: Mapped[List[QueryRequest]] = relationship()
-
-    @property
-    def progress(self):
-        return (self.rows_fetched / self.row_count) * 100
