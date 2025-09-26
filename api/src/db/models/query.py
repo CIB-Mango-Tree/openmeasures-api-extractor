20fc47fc1de0bbf5c7a1f72c8367667ad88430c5
VEDA95 <snetterfield1@gmail.com>
2025-09-26 00:37:10 -0400

    Tweaked byte buffer read in query util func

--- api/src/db/models/query.py
+++ api/src/db/models/query.py
@@ -39,7 +39,7 @@ class Query(BaseWithTimestamp):
         buffer = BytesIO()
         buffer.write(self.processed_data)
 
-        return read_feather(buffer.read())
+        return read_feather(buffer)
 
     @property
     def term(self) -> str:
