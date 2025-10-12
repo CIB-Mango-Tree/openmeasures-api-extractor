1e0b5d1f95982baae71bed1d019b5efeb1a24104
VEDA95 <snetterfield1@gmail.com>
2025-10-11 21:29:44 -0400

    switched to using coroutine for handling async function in all message sending service methods

--- api/src/services/websocket.py
+++ api/src/services/websocket.py
@@ -1,9 +1,7 @@
 from starlette.websockets import WebSocket
 from pyventus.events import EventLinker
 from asyncio import (
-    create_task,
     gather,
-    get_event_loop,
     run_coroutine_threadsafe,
     AbstractEventLoop,
 )
@@ -36,15 +34,14 @@ class WebSocketService:
         @EventLinker.on(FETCH_UPDATE_PROGRESS)
         def handle_progress_update(payload: Event) -> None:
             self.send_by_topic(
-                str(payload.data["id"]),
+                payload.data["id"],
                 {"event": FETCH_UPDATE_PROGRESS, "data": payload.data},
             )
 
         @EventLinker.on(FETCH_INCOMPLETE)
         def handle_incomplete_fetch(payload: Event):
-            logger.debug(f"!!! ABOUT TO EMIT EVENT !!! {FETCH_INCOMPLETE}")
             self.send_by_topic(
-                str(payload.data["id"]),
+                payload.data["id"],
                 {
                     "event": FETCH_INCOMPLETE,
                     "data": {"message": payload.message, "query": payload.data},
@@ -55,7 +52,7 @@ class WebSocketService:
         @EventLinker.on(CLEAN_IN_PROGRESS)
         def handle_in_progress_clean(payload: Event):
             self.send_by_topic(
-                str(payload.data["id"]),
+                payload.data["id"],
                 {
                     "event": CLEAN_IN_PROGRESS,
                     "data": {"message": payload.message, "query": payload.data},
@@ -65,7 +62,7 @@ class WebSocketService:
         @EventLinker.on(PARSE_IN_PROGRESS)
         def handle_in_progress_parse(payload: Event):
             self.send_by_topic(
-                str(payload.data["id"]),
+                payload.data["id"],
                 {
                     "event": PARSE_IN_PROGRESS,
                     "data": {"message": payload.message, "query": payload.data},
@@ -75,7 +72,7 @@ class WebSocketService:
         @EventLinker.on(LIMIT_MAXED_OUT)
         def handle_maxed_limit(payload: Event) -> None:
             self.send_by_topic(
-                str(payload.data["id"]),
+                payload.data["id"],
                 {
                     "event": LIMIT_MAXED_OUT,
                     "data": {"message": payload.message, "query": payload.data},
@@ -85,7 +82,7 @@ class WebSocketService:
         @EventLinker.on(QUERY_COMPLETE)
         def handle_query_complete(payload: Event) -> None:
             self.send_by_topic(
-                str(payload.data["id"]),
+                payload.data["id"],
                 {
                     "event": QUERY_COMPLETE,
                     "data": {"message": payload.message, "query": payload.data},
@@ -140,6 +137,9 @@ class WebSocketService:
         return connection
 
     def broadcast(self, data: Any) -> None:
+        if self._loop is None:
+            return
+
         connections = self._store.get()
 
         async def func():
@@ -151,9 +151,18 @@ class WebSocketService:
                 return_exceptions=True,
             )
 
-        create_task(func())
+        try:
+            routine = run_coroutine_threadsafe(func(), self._loop)
+
+            routine.result()
+
+        except Exception as err:
+            logger.error(err, exc_info=True)
 
     def send(self, id: str, data: Any) -> None:
+        if self._loop is None:
+            return
+
         connection = self._store.get_by_id(id)
 
         if connection is None:
@@ -162,37 +171,40 @@ class WebSocketService:
         async def func() -> None:
             await connection.socket.send_json(data)
 
-        create_task(func())
+        try:
+            routine = run_coroutine_threadsafe(func(), self._loop)
+
+            routine.result()
+
+        except Exception as err:
+            logger.error(err, exc_info=True)
 
     def send_by_topic(self, topic: str, data: Any) -> None:
         if self._loop is None:
             return
 
         connections = self._store.get()
-        subscribed = [connection for connection in connections]
-
-        logger.debug(f"!!! send_by_topic called !!!")
-        logger.debug(f"Topic: {topic}")
-        logger.debug(f"Total connections: {len(connections)}")
-        logger.debug(f"Subscribed connections: {len(subscribed)}")
 
         async def func() -> None:
             if not connections:
                 return
 
-            logger.debug(f"About to send to {len(subscribed)} connections")
             results = await gather(
-                *[connection.socket.send_json(data) for connection in subscribed],
+                *[
+                    connection.socket.send_json(data)
+                    for connection in connections
+                    if connection.topics.has(topic)
+                ],
                 return_exceptions=True,
             )
             logger.debug(f"Send results: {results}")
 
         logger.debug("About to create task")
+
         try:
             routine = run_coroutine_threadsafe(func(), self._loop)
 
-            routine.result(timeout=5)
-            logger.debug("Message sent successfully")
+            routine.result()
 
         except Exception as err:
             logger.error(err, exc_info=True)
