982ec0d6d0ca5a0089d23910603cd62cb2d65dc4
VEDA95 <snetterfield1@gmail.com>
2025-11-06 04:14:40 -0500

    Removed default modifier value in order to make catching empty terms simpler

--- api/src/db/models/term.py
+++ api/src/db/models/term.py
@@ -7,6 +7,6 @@ from .base import BaseModel
 class QueryTerm(BaseModel):
     __tablename__: str = "terms"
     query_id: Mapped[UUID] = mapped_column(ForeignKey("queries.id"), nullable=False)
-    modifier: Mapped[str] = mapped_column(String(8), nullable=False, default="EQUAL")
+    modifier: Mapped[str] = mapped_column(String(8), nullable=False)
     position: Mapped[int] = mapped_column(Integer, nullable=False)
     term: Mapped[str] = mapped_column(Text, nullable=False)
