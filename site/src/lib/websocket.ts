2d991e1f8d7e6af3d76e6f56ea032111cf89456a
VEDA95 <snetterfield1@gmail.com>
2025-11-11 06:23:00 -0500

    Implemented error and reconnection handlers for websocket handler

--- site/src/lib/websocket.ts
+++ site/src/lib/websocket.ts
@@ -1,13 +1,19 @@
 import type { EventMessage, EventMessageData } from '@appTypes/event';
 
 export default class WebSocketConnection {
+  private url: string;
   public socket: WebSocket;
 
   constructor(url: string) {
-    this.socket = new WebSocket(url);
+    this.url = url;
+    this.socket = new WebSocket(this.url);
     this.messageHandler = this.messageHandler.bind(this);
+    this.reconnectHandler = this.reconnectHandler.bind(this);
+    this.errorHandler = this.errorHandler.bind(this);
 
-    this.socket.addEventListener("message", this.messageHandler);
+    this.socket.addEventListener('message', this.messageHandler);
+    this.socket.addEventListener('close', this.reconnectHandler);
+    this.socket.addEventListener('error', this.errorHandler);
   }
 
   private messageHandler(event: MessageEvent): void {
@@ -16,6 +22,20 @@ export default class WebSocketConnection {
     if (message.event == null) return;
 
     this.socket.dispatchEvent(new CustomEvent(message.event, { detail: message.data }));
+  }
+
+  private reconnectHandler(): void {
+    setTimeout((): void => {
+      this.socket = new WebSocket(this.url);
+
+      this.socket.addEventListener('message', this.messageHandler);
+      this.socket.addEventListener('close', this.reconnectHandler);
+      this.socket.addEventListener('error', this.errorHandler);
+    }, 1000);
+  }
+
+  private errorHandler(): void {
+    this.close();
   }
 
   public on(event: string, callback: (data: EventMessageData) => void): void {
@@ -61,6 +81,8 @@ export default class WebSocketConnection {
   }
 
   public close(): void {
+    this.socket.removeEventListener('close', this.reconnectHandler);
+    this.socket.removeEventListener('error', this.errorHandler);
     this.socket.removeEventListener('message', this.messageHandler);
     this.socket.close();
   }
