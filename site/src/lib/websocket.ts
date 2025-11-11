2601128293a651a1088f48fa804db8c36072cd77
VEDA95 <snetterfield1@gmail.com>
2025-11-11 07:24:16 -0500

    Implemented abort controller for removing event listeners when the close method is called

--- site/src/lib/websocket.ts
+++ site/src/lib/websocket.ts
@@ -3,11 +3,13 @@ import type { EventMessage, EventMessageData } from '@appTypes/event';
 export default class WebSocketConnection {
   private url: string;
   private eventManager: EventTarget;
+  private contorller: AbortController;
   public socket: WebSocket;
 
   constructor(url: string) {
     this.url = url;
     this.eventManager = new EventTarget();
+    this.contorller = new AbortController();
     this.socket = new WebSocket(this.url);
     this.messageHandler = this.messageHandler.bind(this);
     this.reconnectHandler = this.reconnectHandler.bind(this);
@@ -41,7 +43,11 @@ export default class WebSocketConnection {
   }
 
   public on(event: string, callback: (data: EventMessageData) => void): void {
-    this.eventManager.addEventListener(event, (event: Event): void => callback((event as CustomEvent).detail as EventMessageData));
+    this.eventManager.addEventListener(
+      event,
+      (event: Event): void => callback((event as CustomEvent).detail as EventMessageData),
+      { signal: this.contorller.signal }
+    );
   }
 
   public off(event: string, callback: (data: any) => void): void {
@@ -86,6 +92,7 @@ export default class WebSocketConnection {
     this.socket.removeEventListener('close', this.reconnectHandler);
     this.socket.removeEventListener('error', this.errorHandler);
     this.socket.removeEventListener('message', this.messageHandler);
+    this.contorller.abort();
     this.socket.close();
   }
 }
