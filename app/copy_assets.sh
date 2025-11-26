d5d922330f7960ae1c4c125bd5b260c0a2ef7f1c
VEDA95 <snetterfield1@gmail.com>
2025-11-26 12:48:21 -0500

    WIP ironing out kinks in github action

--- app/copy_assets.sh
+++ app/copy_assets.sh
@@ -2,4 +2,4 @@
 
 mkdir -p bundle
 cp ../api/dist/mango-tree-api-extractor-backend bundle
-cp -r ../site/.output ../site/.tanstack ../site/.nitro ../site/dist bundle
+cp -r ../site/.output ../site/.nitro ../site/dist bundle
