4c5ff895151e57d1e9feab0a9ab77de35b950070
VEDA95 <snetterfield1@gmail.com>
2026-06-06 16:20:56 -0400

    Fixing CORS issues when using firefox

--- api/main.py
+++ api/main.py
@@ -105,7 +105,12 @@ def main() -> None:
         middleware=[
             Middleware(
                 CORSMiddleware,
-                allow_origins=["*"],
+                allow_origins=[
+                    "http://localhost",
+                    "http://localhost:3000",
+                    "http://127.0.0.1:3000",
+                ],
+                allow_credentials=True,
                 allow_methods=["*"],
                 allow_headers=["*"],
             )
